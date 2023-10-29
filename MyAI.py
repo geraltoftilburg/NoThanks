e249296d91cddbd58b5a5d65f33d0f6a893651be
geraltoftilburg <dogaaras@hotmail.com>
2023-10-29 17:33:06 +0100

    Created MyAI File with needed functions

---
+++ MyAI.py
@@ -0,0 +1,10 @@
+from NoThanksPlayer import Player 
+
+class MyAI(Player): 
+    def __init__( self ): 
+        Player.__init__( self ) 
+        self.setName( 'MyAI' ) 
+
+
+def take( self, card, state ):
+    pass
